298cedd1c62573e1840d9fa7a8d95fbf161e327d
Devfrank94 <francescomurro94@gmail.com>
2023-05-02 17:34:11 +0200

    completed select menu and container cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,7 +12,18 @@ export default {
 <template>
   <body>
     
+    <div class="fmcontainer">
+      <select class="m-3" name="" id="">
+        <option value="Alien">Alien</option>
+      </select>
 
+      <div class="container-fluid p-4">
+
+        <h3>Found 39 cards</h3>
+
+      </div>
+
+    </div>
 
 
 
@@ -31,7 +42,31 @@ export default {
   body{
     background-color: $bg-primary;
     height: 700px; // da rimuovere
+
+    select{
+    border-radius: 5px;
+    padding: 5px;
+    min-width: 150px;
+    border: 0;
   }
+
+  .fmcontainer{
+
+    .container-fluid{
+      background-color: white;
+
+    h3{
+    background-color: $bg-secondary;
+    color: white;
+    font-size: .8rem;
+    font-weight: 700;
+    padding: 1rem;
+    }
+
+    }
+
+  }
+}
 
 
 </style>
